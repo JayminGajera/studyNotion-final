26a27364b48da01d439fb6299b238236c3ad2b3a
JayminGajera <jaymingajera2332@gmail.com>
2023-07-30 13:34:39 +0530

    footer details change

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ CodeHelp © 2023 Studynotion</div>
+          <div className="text-center">Made with ❤️ Jaymin_Gajera © 2023 Studynotion</div>
         </div>
       </div>
     </div>
--- src/services/formatDate.js
+++ src/services/formatDate.js
@@ -1,4 +1,5 @@
 export const formatDate = (dateString) => {
+  
   const options = { year: "numeric", month: "long", day: "numeric" }
   const date = new Date(dateString)
   const formattedDate = date.toLocaleDateString("en-US", options)
